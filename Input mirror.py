5560ce7530a4288dbc63db00b90ba80944d167e1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-11 13:30:40 +0600

    Create Input mirror.py

---
+++ Input mirror.py
@@ -0,0 +1,12 @@
+# write a simple program - the Input mirror. Your program needs to do the following
+
+# Accept an integer input from the console and store it in the variable N
+
+# Accept a string input from the console and store it in the variable S
+
+# Output the integer first and then string on the same line
+
+n=5
+s="abcde"
+print(n)
+print(s)
