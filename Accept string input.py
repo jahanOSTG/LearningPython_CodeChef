b89044732f4aca4910e098bf8eeb9ff93a5fecc8
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-11 13:26:25 +0600

    Create Accept string input.py

---
+++ Accept string input.py
@@ -0,0 +1,15 @@
+#You need to write a program which does the following
+
+    #• Accepts 2 space separated alphanumeric strings as input in 1st line as the variables A, В
+
+    #• Accepts 3 space separated alphanumeric strings as input in 2nd line as the variables C, D, E
+
+    #• Accepts 4 space separated alphanumeric strings as input in 3rd line as the variables F, G, H, I
+
+    #• Prints out 9 space separated strings as output in a single line to the console
+
+
+A, B = input().split()
+C, D, E = input().split()
+F, G, H, I = input().split()
+print(A, B, C, D, E, F, G, H, I)
