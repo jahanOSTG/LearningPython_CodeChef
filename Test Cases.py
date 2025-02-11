a6b3b77bfd4105185bc471d7a1a457685fbb3422
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-11 22:12:21 +0600

    Create Test Cases.py

---
+++ Test Cases.py
@@ -0,0 +1,15 @@
+# Write a program 
+# Accepts 5 inputs given on 5 separate lines. Each input is an integer N
+# For each test cases, prints out the integer N
+# N to console on a separate line
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
