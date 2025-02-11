7623df264688a92a56ae8ba0d006cea309299955
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-11 12:42:54 +0600

    Update Area & Perimeter of Rectangle.py

--- Area & Perimeter of Rectangle.py
+++ Area & Perimeter of Rectangle.py
@@ -1,3 +1,13 @@
+# Define length and breadth of the rectangle
+# length = 11
+# breadth = 13
+
+# Calculate area using the formula: area = length * breadth
+# area = 11 * 13
+
+# Calculate perimeter using the formula: perimeter = 2 * (length + breadth)
+# perimeter = 2 * (11 + 13)
+
 
 
 print(11 * 13)          # Area of the rectangle
